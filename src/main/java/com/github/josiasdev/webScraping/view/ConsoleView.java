2c54e4282377cfa347769307a98d2a6ba426ce56
josiasdev <francisco.batista67@alu.ufc.br>
2025-03-31 16:14:33 -0300

    feat: responsável por mostrar mensagem e exibir erro

---
+++ src/main/java/com/github/josiasdev/webScraping/view/ConsoleView.java
@@ -0,0 +1,11 @@
+package com.github.josiasdev.webScraping.view;
+
+public class ConsoleView {
+    public void exibirMensagem(String mensagem) {
+        System.out.println(mensagem);
+    }
+
+    public void exibirErro(String erro) {
+        System.err.println(erro);
+    }
+}
