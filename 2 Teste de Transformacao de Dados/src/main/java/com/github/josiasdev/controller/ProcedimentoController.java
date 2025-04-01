c2c8aae29cec77d4bebf14d5f0724f76cbd6ed88
josiasdev <josiasmartins098@gmail.com>
2025-04-01 14:20:18 -0300

    feat: processamento da extração dos dados do pdf

--- 2 Teste de Transformacao de Dados/src/main/java/com/github/josiasdev/controller/ProcedimentoController.java
+++ 2 Teste de Transformacao de Dados/src/main/java/com/github/josiasdev/controller/ProcedimentoController.java
@@ -1,4 +1,24 @@
 package com.github.josiasdev.controller;
 
+import com.github.josiasdev.service.ProcedimentoService;
+import com.github.josiasdev.view.ConsoleView;
+
 public class ProcedimentoController {
+    private final ProcedimentoService procedimentoService;
+    private final ConsoleView view;
+
+    public ProcedimentoController(ProcedimentoService procedimentoService, ConsoleView view){
+        this.procedimentoService = procedimentoService;
+        this.view = view;
+    }
+    public void processar(){
+        if(procedimentoService.extrairDadosPdf("Anexo I.pdf")){
+            view.exibirMensagem("Dados extraidos com sucesso!");
+            procedimentoService.compactarParaZip("teste_{josias}.zip", "rol_procedimento.csv");
+            view.exibirMensagem("Arquivo compactado com sucesso!");
+        }
+        else{
+            view.exibirErro("Não foi possivel encontrar o Anexo I.pdf");
+        }
+    }
 }
