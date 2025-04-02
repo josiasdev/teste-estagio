ec65464a51031ec585bf7e25ab569c94f8d81db2
josiasdev <josiasmartins098@gmail.com>
2025-04-02 15:06:30 -0300

    feat: função para extrair os textos do pdf refeita usando pdfBox e o Tabula

--- 2 Teste de Transformacao de Dados/src/main/java/com/github/josiasdev/service/ProcedimentoService.java
+++ 2 Teste de Transformacao de Dados/src/main/java/com/github/josiasdev/service/ProcedimentoService.java
@@ -1,100 +1,93 @@
 package com.github.josiasdev.service;
 
-import com.aspose.pdf.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import technology.tabula.ObjectExtractor;
+import technology.tabula.Page;
+import technology.tabula.RectangularTextContainer;
+import technology.tabula.Table;
+import technology.tabula.extractors.SpreadsheetExtractionAlgorithm;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import com.aspose.zip.Archive;
-import com.aspose.zip.ArchiveEntrySettings;
 
-public class ProcedimentoService {
+public class ProcedimentoService{
+    static final int TAMANHO_BUFFER = 4096;
 
-    public boolean extrairDadosPdf(String filename) {
-        try {
-            Document pdfDocument = new Document(filename);
-            TableAbsorber absorber = new TableAbsorber();
+    public boolean extrairTextoDoPDF(String filename) {
+        try (PDDocument document = PDDocument.load(new File(filename))) {
+            ObjectExtractor extractor = new ObjectExtractor(document);
+            SpreadsheetExtractionAlgorithm sea = new SpreadsheetExtractionAlgorithm();
+            List<Page> pages = new ArrayList<>();
+            extractor.extract().forEachRemaining(pages::add);
+
             StringBuilder csvContent = new StringBuilder();
-            for (Page page : pdfDocument.getPages()){
-                absorber.visit(page);
+            for (Page page : pages) {
+                List<Table> tables = sea.extract(page);
 
-                    for (AbsorbedTable table : absorber.getTableList()) {
-                        List<Integer> targetColumns = new ArrayList<>();
-                        if (!table.getRowList().isEmpty()) {
-                            AbsorbedRow headerRow = table.getRowList().get(0);
-                            int columnIndex = 0;
-                            for (AbsorbedCell cell : headerRow.getCellList()) {
-                                StringBuilder headerTextBuilder = new StringBuilder();
-                                for (TextFragment fragment : cell.getTextFragments()) {
-                                    for (TextSegment seg : fragment.getSegments()) {
-                                        headerTextBuilder.append(seg.getText().trim()).append(" ");
-                                    }
-                                }
-                                String headerText = headerTextBuilder.toString().trim();
-                                if (headerText.equalsIgnoreCase("OD") || headerText.equalsIgnoreCase("AMB")) {
-                                    targetColumns.add(columnIndex);
-                                }
-                                columnIndex++;
+                for (Table table : tables) {
+                    for (List<RectangularTextContainer> row : table.getRows()){
+                        StringBuilder csvRow = new StringBuilder();
+                        for (RectangularTextContainer cell : row){
+                            String cellText = cell.getText().trim().replace("\"", "''");
+                            if (cellText.equals("OD")) {
+                                cellText = "Seg. Odontológica";
+                            } else if (cellText.equals("AMB")) {
+                                cellText = "Seg. Ambulatorial";
                             }
+                            csvRow.append('"').append(cellText).append('"').append(",");
                         }
-
-                        for (AbsorbedRow row : table.getRowList()) {
-                            StringBuilder csvRow = new StringBuilder();
-                            int columnIndex = 0;
-                            for (AbsorbedCell cell : row.getCellList()) {
-                                StringBuilder sb = new StringBuilder();
-                                for (TextFragment fragment : cell.getTextFragments()) {
-                                    for (TextSegment seg : fragment.getSegments()) {
-                                       String text = seg.getText().trim();
-                                        if (targetColumns.contains(columnIndex)) {
-                                            text = text.replace("OD", "Seg. Odontológica")
-                                                    .replace("AMB", "Seg. Ambulatorial");
-                                        }
-                                        sb.append(text).append(" ");
-                                    }
-                                }
-                                String cellText = sb.toString().trim().replace("\"", "'");
-                                csvRow.append('"').append(cellText).append('"').append(",");
-                                columnIndex++;
-                            }
-                            if (csvRow.length() > 0) {
-                                csvContent.append(csvRow.substring(0, csvRow.length() - 1)).append("\n"); // Remove última vírgula
-                            }
+                        if (csvRow.length() > 0){
+                            csvContent.append(csvRow.substring(0, csvRow.length() - 1)).append("\n");
                         }
                     }
+                }
             }
             if (csvContent.length() > 0) {
                 salvarCsv("rol_procedimento.csv", csvContent.toString());
                 return true;
-            }
-            else{
+            } else {
                 return false;
             }
-        } catch (Exception e) {
+        } catch (Exception e){
             e.printStackTrace();
             return false;
         }
     }
+
     private void salvarCsv(String filename, String content) {
         try (FileWriter writer = new FileWriter(filename)) {
             writer.write(content);
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
     }
 
-    public void compactarZip(){
-        try(FileOutputStream zipFile = new FileOutputStream("teste_{josias}.zip")){
-            try (FileInputStream source1 = new FileInputStream("rol_procedimento.csv")){
-                try (Archive archive = new Archive(new ArchiveEntrySettings())){
-                    archive.createEntry("rol_procedimento.csv", source1);
-                    archive.save(zipFile);
-                }
-            } catch (IOException e){
-                e.printStackTrace();
+    public void compactarParaZip(String arqSaida, String arqEntrada){
+        int cont;
+        byte[] dados = new byte[TAMANHO_BUFFER];
+        BufferedInputStream origem = null;
+        FileInputStream streamEntrada = null;
+        FileOutputStream destino = null;
+        ZipOutputStream saida = null;
+        ZipEntry entry = null;
+        try{
+            destino = new FileOutputStream(new File(arqSaida));
+            saida = new ZipOutputStream(new BufferedOutputStream(destino));
+            File file = new File(arqEntrada);
+            streamEntrada = new FileInputStream(file);
+            origem = new BufferedInputStream(streamEntrada, TAMANHO_BUFFER);
+            entry = new ZipEntry(file.getName());
+            saida.putNextEntry(entry);
+            while ((cont = origem.read(dados, 0, TAMANHO_BUFFER)) != -1){
+                saida.write(dados, 0, cont);
             }
-        } catch (IOException e){
+            origem.close();
+            saida.close();
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
