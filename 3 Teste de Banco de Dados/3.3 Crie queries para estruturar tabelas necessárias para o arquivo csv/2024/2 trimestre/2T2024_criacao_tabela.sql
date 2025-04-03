e390a1072e57a6177216518d56c37e80677b75ef
josiasdev <josiasmartins098@gmail.com>
2025-04-03 14:00:00 -0300

    feat: Criação de tabelas de 2024

---
+++ 3 Teste de Banco de Dados/3.3 Crie queries para estruturar tabelas necessárias para o arquivo csv/2024/2 trimestre/2T2024_criacao_tabela.sql
@@ -0,0 +1,8 @@
+CREATE table segundoTrimestre2024 (
+        data_ DATE,
+        reg_ans VARCHAR(10),
+        cd_conta_contabil VARCHAR(20),
+        descricao VARCHAR(255),
+        vl_saldo_inicial VARCHAR(20),
+        Vl_saldo_final VARCHAR(20)
+);
